f17516c9e2e6c45101b9d6a22b11a7a410ffbe07
angelostori <angelostori13@gmail.com>
2025-10-28 09:51:29 +0100

    add new milestones

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,3 +85,30 @@ function newCard(obj) {
 
 //const myObj = [{id: 1, title: 'Skate Park', date: '01-07-2024', url: 'https://marcolanci.it/boolean/assets/pictures/1.png'}]
 //newCard(myObj[0])
+
+/*
+Milestone 1
+
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque 
+ed un button di chiusura.
+
+Milestone 2
+
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+
+Milestone 3
+
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto 
+a essere mostrata all’interno dell’overlay.
+
+Ci sono diversi modi di farlo, prova a sperimentare 🙂
+
+Bonus
+
+Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà 
+fluida. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
+*/
